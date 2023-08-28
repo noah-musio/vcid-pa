609a90b2fcce97f9959f741c6607d10c4bcc75b0
noah-musio <noah@musio.tv>
2023-08-28 15:25:37 +0200

    minor changes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -26,6 +26,8 @@ def data():
 def get_years():
     years = db.session.query(func.extract('year', Balance.date)).distinct().all()
     return [year[0] for year in years] 
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -65,15 +67,14 @@ def register():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
-    form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url, form=form)
+    # posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        # page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    # next_url = url_for('user', username=user.username, page=posts.next_num) \
+        # if posts.has_next else None
+    #    if posts.has_prev else None
+    # form = EmptyForm()
+    # return render_template('user.html', user=user, posts=posts.items,
+    # next_url=next_url, prev_url=prev_url, form=form)
 
 
 @app.route('/logout')
